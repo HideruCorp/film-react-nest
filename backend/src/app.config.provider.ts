862a1f6b99c453b09af88c8b41006f00f2339f88
Aziev Damir <hideru.a-dam@yandex.com>
2026-03-07 18:07:20 +0500

    feat(config): implement environment variable reading with useFactory
    - app.config.provider.ts - заменен useValue на useFactory для чтения переменных среды через ConfigService, добавлен импорт ConfigService, реализована инъекция зависимостей
    - yarn.lock - обновлены URL-адреса реестра пакетов с registry.yarnpkg.com на registry.npmjs.org

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -1,11 +1,15 @@
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 export const configProvider = {
     imports: [ConfigModule.forRoot()],
     provide: 'CONFIG',
-    useValue: < AppConfig> {
-        //TODO прочесть переменнные среды
-    },
+    useFactory: (configService: ConfigService): AppConfig => ({
+        database: {
+            driver: configService.get<string>('DATABASE_DRIVER', 'mongodb'),
+            url: configService.get<string>('DATABASE_URL', 'mongodb://localhost:27017/prac'),
+        }
+    }),
+    inject: [ConfigService],
 }
 
 export interface AppConfig {
